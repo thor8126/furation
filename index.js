2d3c1ac7823d5b10198530b71467519fb0f170ca
Lakhvinder <lakhabhayi@gmail.com>
2023-06-19 22:06:12 +0530

    delpoyed

--- index.js
+++ index.js
@@ -13,7 +13,31 @@ app.use(bodyParser.json());
 
 // Routes
 app.get("/", (req, res) => {
-  res.send("Api by Lakhvinder Singh!");
+  const string = `
+  Api by Lakhvinder Singh!,
+
+
+1.  To retrieve all items from the database, send a GET request to
+
+        https:///furation.onrender.com/api/items.
+
+2.  To retrieve a specific item by its ID, send a GET request to below url, replacing :id with the actual item ID.
+
+        https:///furation.onrender.com/api/items/:id
+
+3.  To create a new item, send a POST request to below url with a JSON payload containing the item data.
+
+        https:///furation.onrender.com/api/items
+
+4.  To update an existing item by its ID, send a PUT request to below url , replacing :id with the actual item ID, and include the updated item data in the request body.
+
+        https:///furation.onrender.com/api/items/:id
+
+5.  To delete an item by its ID, send a DELETE request to below url, replacing :id with the actual item ID.
+
+        https:///furation.onrender.com/api/items/:id`;
+
+  res.send(string);
 });
 
 app.use("/", require("./routes/crudRoutes"));
